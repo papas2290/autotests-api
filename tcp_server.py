a414ea9ddbfeeedea65e65e134a3ed9f1918f5a0
papas2290 <papas2290@mail.ru>
2026-06-11 16:57:07 +0200

    Знакомство с WebSocket протоколом

--- tcp_server.py
+++ tcp_server.py
@@ -8,19 +8,31 @@ def server():
     server_socket.bind(server_address)
 
     server_socket.listen(5)
+    server_socket.settimeout(1.0)
     print('Сервер запущен и ждет подключений...')
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f'Подключение от {client_address}')
+    try:
+        while True:
+            try:
+                client_socket, client_address = server_socket.accept()
+            except socket.timeout:
+                continue
+            print(f'Подключение от {client_address}')
 
-        data = client_socket.recv(1024).decode()
-        print(f'Получено сообщение: {data}')
+            data = client_socket.recv(1024).decode()
+            print(f'Получено сообщение: {data}')
 
-        response = f'Сервер получил: {data}'
-        client_socket.send(response.encode())
+            response = f'Сервер получил: {data}'
+            client_socket.send(response.encode())
 
-        client_socket.close()
+            client_socket.close()
+
+    except KeyboardInterrupt:
+        print('Сервер остановлен')
+
+    finally:
+        server_socket.close()
+
 
 if __name__ == '__main__':
     server()
