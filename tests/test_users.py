f7b5d4117af951b4a4e420eb3828b23c3ad8b5ab
papas2290 <papas2290@mail.ru>
2026-06-19 13:12:14 +0200

    Маркировки Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+import pytest
+
 from clients.users.public_users_client import get_public_users_client
 from clients.users.user_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from http import HTTPStatus
@@ -7,6 +9,8 @@ from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
 
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_user_client = get_public_users_client()
     request = CreateUserRequestSchema()
