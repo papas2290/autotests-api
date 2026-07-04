1136f4ff17b09d2f531b2b3c4cdd18890447b080
papas2290 <papas2290@mail.ru>
2026-07-04 15:36:30 +0200

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Удаление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     GetExerciseResponseSchema, GetExercisesResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
@@ -10,7 +11,7 @@ from fixtures.exercises import ExercisesFixture
 
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -51,3 +52,16 @@ class TestExercises:
         assert_update_exercise_response(request=request, response=response_data, exercise_id=exercise_id)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExercisesFixture):
+        exercise_id = function_exercise.response.exercise.id
+        response_delete = exercises_client.delete_exercise_api(exercise_id=exercise_id)
+        assert_status_code(response_delete.status_code, HTTPStatus.OK)
+
+        response_get = exercises_client.get_exercise_api(exercise_id=exercise_id)
+        response_get_data = InternalErrorResponseSchema.model_validate_json(response_get.text)
+
+        assert_status_code(response_get.status_code, HTTPStatus.NOT_FOUND)
+        assert_exercise_not_found_response(actual=response_get_data)
+
+        validate_json_schema(instance=response_get.json(), schema=response_get_data.model_json_schema())
