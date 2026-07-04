cd8cf8fda8e99db261422a689173fac2de675718
papas2290 <papas2290@mail.ru>
2026-07-04 15:00:36 +0200

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,11 +3,13 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema, GetExercisesResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExercisesFixture
 
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -23,4 +25,15 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request=request, response=response_data)
 
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExercisesFixture):
+        response = exercises_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(
+            get_exercise_response=response_data,
+            create_exercise_response=function_exercise.response
+        )
         validate_json_schema(response.json(), response_data.model_json_schema())
