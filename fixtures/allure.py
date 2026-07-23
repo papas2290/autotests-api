ff6909f5f066b5d2c80d81bc2d1e38443bd785cc
papas2290 <papas2290@users.noreply.github.com>
2026-07-23 19:20:15 +0000

    deploy: 8fc47c8c80c2aed306a7f6b3bfa5967e271c6ced

--- fixtures/allure.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from tools.allure.environment import create_allure_environment_file
-
-
-@pytest.fixture(autouse=True, scope='session')
-def save_allure_environment_file():
-    yield
-    create_allure_environment_file()
--- fixtures/authentication.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
